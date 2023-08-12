eb66236a4f5e6556790e8baac9b45ade79828c47
NahidNml <fuad.allpic@gmail.com>
2023-08-12 18:34:50 +0600

    12Aug2023

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBookWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        //public void Save()
+        //{
+        //    _db.SaveChanges();
+        //}
+
+        //public void Update(ApplicationUser obj)
+        //{
+        //    _db.ApplicationUsers.Update(obj);
+        //}
+    }
+}
