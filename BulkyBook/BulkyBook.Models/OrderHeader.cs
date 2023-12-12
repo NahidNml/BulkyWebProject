5375ecb99c7674f06948a424dd0458d3a5a586fb
NahidNml <fuad.allpic@gmail.com>
2023-12-12 22:31:24 +0600

    10(8)

--- BulkyBook/BulkyBook.Models/OrderHeader.cs
+++ BulkyBook/BulkyBook.Models/OrderHeader.cs
@@ -12,7 +12,7 @@ namespace BulkyBook.Models
     public class OrderHeader
     {
         public int Id { get; set; }
-
+        public string ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
         [ValidateNever]
         public ApplicationUser ApplicationUser { get; set; }
--- BulkyBook/BulkyBook.Models/ViewModels/ShoppingCartVM.cs
+++ BulkyBook/BulkyBook.Models/ViewModels/ShoppingCartVM.cs
@@ -9,6 +9,9 @@ namespace BulkyBook.Models.ViewModels
 	public class ShoppingCartVM
 	{
 		public IEnumerable<ShoppingCart> ListCart { get; set; }
-		public double CartTotal { get; set; }
+
+		//public double CartTotal { get; set; }
+
+		public OrderHeader OrderHeader { get; set; }
 	}
 }
